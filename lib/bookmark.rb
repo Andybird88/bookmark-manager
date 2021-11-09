6602d137697070472caf7e3ee6e9af1e4d9a91b5
Myoung <mhbae93@gmail.com>
2021-11-10 01:22:09 +0900

    testing pg

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,9 +1,9 @@
-class Bookmark
+require 'pg'
 
+class Bookmark
   def self.all
-    connection = PG.connect(dbname:'bookmark_manager')
-    result = connection.exec('SELECT * FROM bookmarks')
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec('SELECT * FROM bookmarks;')
     result.each { |bookmark| bookmark['url'] }
   end
-
 end
