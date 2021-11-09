e3ea568459b279c1c60640e3a5671e28b7f52caf
Tatiana Nantes <tatiananantes@gmail.com>
2021-11-09 16:08:21 +0000

    implementing pg to retrieve bookmarks from db

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -2,12 +2,6 @@ require 'bookmark'
 
 describe Bookmark do
   
-  it 'initializes with a name and url' do
-    bookmark = Bookmark.new('BBC', 'https://bbc.co.uk/')
-    expect(bookmark.name).to eq('BBC')
-    expect(bookmark.url).to eq('https://bbc.co.uk/')
-  end
-
   describe '.all' do 
     it 'returns all bookmarks' do
       bookmarks = Bookmark.all
